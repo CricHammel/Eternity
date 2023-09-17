e89ff53d4949b3be043e195ddd1fed2e8c2c8839
Cric_Hammel <cric_hammel@gmx.de>
2023-09-17 16:42:53 -0300

    Added KreeArmor
    - Added the first implementation of a CustomTieredArmor, big changes are
    coming!

---
+++ Eternity/src/main/java/de/cric_hammel/eternity/infinity/items/kree/KreeArmor.java
@@ -0,0 +1,32 @@
+package de.cric_hammel.eternity.infinity.items.kree;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+
+import de.cric_hammel.eternity.infinity.items.CustomArmor;
+import de.cric_hammel.eternity.infinity.items.CustomTieredArmor;
+
+public class KreeArmor extends CustomTieredArmor implements Listener {
+
+	public KreeArmor() {
+		super(CustomArmor.ArmorType.CHAINMAIL, ChatColor.RED + "Kree", "The Armor of the mighty Kree");
+	}
+
+	@Override
+	public void changeTierOne() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeTierTwo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeTierThree() {
+		// TODO Auto-generated method stub
+		
+	}
+}
